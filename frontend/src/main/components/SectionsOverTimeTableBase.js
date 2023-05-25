748265cd1d830ec7b6959a0d1c5d2077f90b7996
Brenna <brennascholte@ucsb.edu>
2023-05-24 22:51:13 -0700

    bs - changed colors in course history table

--- frontend/src/main/components/SectionsOverTimeTableBase.js
+++ frontend/src/main/components/SectionsOverTimeTableBase.js
@@ -38,7 +38,7 @@ export default function SectionsOverTimeTableBase({ columns, data, testid = "tes
                     {...cell.getCellProps()}
                     data-testid={`${testid}-cell-row-${cell.row.index}-col-${cell.column.id}`}
                     // Stryker disable next-line ObjectLiteral
-                    style={{background: cell.isGrouped ? "#e5fcf4" : cell.isAggregated ? "#e5fcf4" : "#effcf8", fontWeight: cell.isGrouped ? "bold" : cell.isAggregated ? "bold" : "normal"}}
+                    style={{background: cell.isGrouped ? "#003660" : cell.isAggregated ? "#003660" : "#7490a6", color: cell.isGrouped ? "#effcf4" : cell.isAggregated ? "#effcf4" : "#000000", fontWeight: cell.isGrouped ? "bold" : cell.isAggregated ? "bold" : "normal"}}
                     >
                     
                     {cell.isGrouped ? (
