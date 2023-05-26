eed3ffb789fe0d4174d8775d9d635bc6232789fb
Yiu-Shing Li <li12@umail.ucsb.edu>
2023-05-26 01:33:28 -0700

    yl - WIP attempted implementation for controller; tests failing

---
+++ src/main/java/edu/ucsb/cs156/courses/controllers/CourseOverTimeInstructorController.java
@@ -0,0 +1,47 @@
+package edu.ucsb.cs156.courses.controllers;
+
+import java.util.List;
+
+import edu.ucsb.cs156.courses.collections.ConvertedSectionCollection;
+import edu.ucsb.cs156.courses.documents.ConvertedSection;
+import io.swagger.annotations.ApiOperation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping("/api/public/courseovertime")
+public class CourseOverTimeInstructorController {
+
+    private final Logger logger = LoggerFactory.getLogger(CourseOverTimeInstructorController.class);
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    ConvertedSectionCollection convertedSectionCollection;
+
+    @ApiOperation(value = "Get a list of courses over time")
+    @GetMapping(value = "/instructorsearch", produces = "application/json")
+    public ResponseEntity<String> search(
+        @RequestParam String startQtr,
+        @RequestParam String endQtr,
+        @RequestParam String instructor
+    ) throws JsonProcessingException {
+        List<ConvertedSection> courseResults = convertedSectionCollection.findByQuarterRangeAndInstructor(
+            startQtr,
+            endQtr,
+            instructor
+        );
+        String body = mapper.writeValueAsString(courseResults);
+        return ResponseEntity.ok().body(body);
+    }    
+}
