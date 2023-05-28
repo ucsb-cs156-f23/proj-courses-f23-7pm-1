e62e2ff6f2ff917e03a7ebd63e6ffd77f4e84c89
Yiu-Shing Li <li12@umail.ucsb.edu>
2023-05-28 15:46:46 -0700

    yl - add ApiParam annotation according to Kenny's example at #19

--- src/main/java/edu/ucsb/cs156/courses/controllers/CourseOverTimeInstructorController.java
+++ src/main/java/edu/ucsb/cs156/courses/controllers/CourseOverTimeInstructorController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiParam;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -32,8 +34,28 @@ public class CourseOverTimeInstructorController {
     @ApiOperation(value = "Get a list of courses over time, filtered by instructor name")
     @GetMapping(value = "/instructorsearch", produces = "application/json")
     public ResponseEntity<String> search(
+        @ApiParam(
+            name = "startQtr",
+            type = "String",
+            value = "Starting quarter in yyyyq format, e.g. 20231 for W23, 20232 for S23, etc. (1=Winter, 2=Spring, 3=Summer, 4=Fall)",
+            example = "20231",
+            required = true
+        )
         @RequestParam String startQtr,
+        @ApiParam(
+            name =  "endQtr",
+            type = "String",
+            value = "Ending quarter in yyyyq format, e.g. 20231 for W23, 20232 for S23, etc. (1=Winter, 2=Spring, 3=Summer, 4=Fall)",
+            example = "20231",
+            required = true
+        )
         @RequestParam String endQtr,
+        @ApiParam(
+            name =  "instructor",
+            type = "String",
+            value = "Instructor name; e.g. 'conrad' or 'CONRAD' or 'CONRAD P T'",
+            example = "CONRAD",
+            required = true)
         @RequestParam String instructor
     ) throws JsonProcessingException {
         List<ConvertedSection> courseResults = convertedSectionCollection.findByQuarterRangeAndInstructor(
