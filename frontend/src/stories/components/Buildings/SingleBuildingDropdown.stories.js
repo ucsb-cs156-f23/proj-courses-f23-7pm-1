91797f120e92b098171bcf48a4bcd118033cc681
khtran1 <khtran@ucsb.edu>
2023-06-02 17:47:27 -0700

    kt - add dropdown files

---
+++ frontend/src/stories/components/Buildings/SingleBuildingDropdown.stories.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import SingleBuildingDropdown from "main/components/Buildings/SingleBuildingDropdown";
+import {oneBuilding, threeBuildings, allBuildings} from "fixtures/buildingFixtures";
+
+export default {
+    title: 'components/Buildings/SingleBuildingDropdown',
+    component: SingleBuildingDropdown
+};
+
+const Template = (args) => {
+    const [buildings, setBuilding] = useState(args.buildings[0]);
+
+    return (
+        < SingleBuildingDropdown 
+        buildings={buildings} 
+        setBuilding={setBuilding} 
+        controlId={"SampleControlId"}
+        label={"Building"} 
+        {...args} />
+    )
+};
+
+export const OneBuilding = Template.bind({});
+OneBuilding.args = {
+    buildings: oneBuilding
+};
+
+export const ThreeBuildings = Template.bind({});
+ThreeBuildings.args = {
+    buildings: threeBuildings
+};
+
+export const AllBuildings = Template.bind({});
+AllBuildings.args = {
+    buildings: allBuildings
+};
+
