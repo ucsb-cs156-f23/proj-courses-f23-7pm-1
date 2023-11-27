0de325d123e460796c468adecf50d36d6e4bd8e5
Carson Kopper <ckopper@ucsb.edu>
2023-11-27 14:51:07 -0800

    ck - created UpdateFinalsDataJob

--- src/main/java/edu/ucsb/cs156/courses/documents/FinalExam.java
+++ src/main/java/edu/ucsb/cs156/courses/documents/FinalExam.java
@@ -9,11 +9,18 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class FinalExam {
+public class FinalExam implements Cloneable {
   private boolean hasFinals;
   private String comments;
   private String examDay;
   private String examDate;
   private String beginTime;
   private String endTime;
+
+  public Object clone() throws CloneNotSupportedException {
+
+    FinalExam newFinalExam = (FinalExam) super.clone();
+
+    return newFinalExam;
+  }
 }
---
+++ src/main/java/edu/ucsb/cs156/courses/jobs/UpdateFinalsDataJob.java
@@ -0,0 +1,68 @@
+package edu.ucsb.cs156.courses.jobs;
+
+import edu.ucsb.cs156.courses.collections.ConvertedSectionCollection;
+import edu.ucsb.cs156.courses.documents.ConvertedSection;
+import edu.ucsb.cs156.courses.documents.CoursePage;
+import edu.ucsb.cs156.courses.documents.FinalExam;
+import edu.ucsb.cs156.courses.models.Quarter;
+import edu.ucsb.cs156.courses.services.UCSBCurriculumService;
+import edu.ucsb.cs156.courses.services.jobs.JobContext;
+import edu.ucsb.cs156.courses.services.jobs.JobContextConsumer;
+import java.util.List;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UpdateFinalsDataJob implements JobContextConsumer {
+  private String start_quarterYYYYQ;
+  private String end_quarterYYYYQ;
+  private List<String> subjects;
+  private UCSBCurriculumService ucsbCurriculumService;
+  private ConvertedSectionCollection convertedSectionCollection;
+
+  @Override
+  public void accept(JobContext ctx) throws Exception {
+    List<Quarter> quarters = Quarter.quarterList(start_quarterYYYYQ, end_quarterYYYYQ);
+    for (Quarter quarter : quarters) {
+      String quarterYYYYQ = quarter.getYYYYQ();
+      updateFinals(ctx, quarterYYYYQ);
+    }
+  }
+
+  public void updateFinals(JobContext ctx, String quarterYYYYQ)
+      throws Exception {
+    ctx.log("Updating final exam info for [" + quarterYYYYQ + "]");
+
+    List<ConvertedSection> convertedSections =
+        convertedSectionCollection.findAll();
+
+    ctx.log("Attempting to update final exams for " + convertedSections.size() + " sections in MongoDB...");
+
+    int updatedSections = 0;
+    int errors = 0;
+
+    for (ConvertedSection section : convertedSectionCollection.findAll()) {
+      String quarter = section.getCourseInfo().getQuarter();
+      String enrollCd = section.getSection().getEnrollCode();
+      if (quarter.equals(quarterYYYYQ)) {
+        try {
+          FinalExam finalExam = ucsbCurriculumService.getFinalExamObject(quarter, enrollCd);
+          section.setFinalExam(finalExam);
+          convertedSectionCollection.save(section);
+          updatedSections++;
+        } catch (Exception e) {
+          ctx.log("Error saving final exam: " + e.getMessage());
+          errors++;
+        }
+      }
+    }
+
+    ctx.log(
+        String.format(
+            "%d final exams updated, %d errors",
+            updatedSections, errors));
+    ctx.log("Final exam info for [" + quarterYYYYQ + "] has been updated");
+  }
+}
