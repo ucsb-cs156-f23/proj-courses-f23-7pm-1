72e975643deb0f66b23a5cb98cd794397a9ec11f
Carson Kopper <ckopper@ucsb.edu>
2023-11-27 17:19:30 -0800

    ck - updated JobsController tests

--- src/test/java/edu/ucsb/cs156/courses/controllers/JobsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/courses/controllers/JobsControllerTests.java
@@ -287,4 +287,24 @@ public class JobsControllerTests extends ControllerTestCase {
 
     assertNotNull(jobReturned.getStatus());
   }
+
+  @WithMockUser(roles = {"ADMIN"})
+  @Test
+  public void admin_can_launch_update_finals_data_job() throws Exception {
+    // act
+    MvcResult response =
+        mockMvc
+            .perform(
+                post("/api/jobs/launch/updateFinalsData?start_quarterYYYYQ=20221&end_quarterYYYYQ=20222")
+                    .with(csrf()))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    // assert
+    String responseString = response.getResponse().getContentAsString();
+    log.info("responseString={}", responseString);
+    Job jobReturned = objectMapper.readValue(responseString, Job.class);
+
+    assertNotNull(jobReturned.getStatus());
+  }
 }
